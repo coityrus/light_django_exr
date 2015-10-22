f16d3fc9458521893ab51350e9dc30a838c00652
Sung-nyoun Kim <tityrus@gmail.com>
2015-10-22 16:32:32 +0900

    end of chapter 2

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -1,11 +1,20 @@
 import hashlib
 import os
 import sys
+
+from io import BytesIO
+from PIL import Image, ImageDraw
+
 from django.conf import settings
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
 SECRET_KEY = os.environ.get('SECRET_KEY', '%3_v6cidbq@4z^+$b6no9!bm*lgff^%+pu3qyj@p$w@-1mie!+')
+
+BASE_DIR = os.path.dirname(__file__)
+
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
 settings.configure(
     DEBUG=DEBUG,
     SECRET_KEY=SECRET_KEY,
@@ -16,15 +25,26 @@ settings.configure(
             'django.middleware.csrf.CsrfViewMiddleware',
             'django.middleware.clickjacking.XFrameOptionsMiddleware',
             ),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+    ),
+    TEMPLATE_DIRS=(
+        os.path.join(BASE_DIR, 'templates'),
+    ),
+    STATICFILES_DIRS=(
+        os.path.join(BASE_DIR, 'static'),
+    ),
+    STATIC_URL='/static/',
 )
 
-from io import BytesIO
-from PIL import Image, ImageDraw
+
 from django import forms
 from django.conf.urls import url
-from djang.core.cache import cache
+from django.core.cache import cache
+from django.core.urlresolvers import reverse
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render
 from django.views.decorators.http import etag
 
 class ImageForm(forms.Form):
@@ -44,11 +64,11 @@ class ImageForm(forms.Form):
             if textwidth < width and textheight < height:
                 texttop = (height - textheight) // 2
                 textleft = (width - textwidth) // 2
-                draw.text((textleft, texttop), text, fill=(255,255,255))
+                draw.text((textleft, texttop), text, fill=(255, 255, 255))
             content = BytesIO()
             image.save(content, image_format)
             content.seek(0)
-            cache.set(key, content, 60 * 60) # cache for one hour
+            cache.set(key, content, 60 * 60) # cache persists for one hour
         return content
 
 def generate_etag(request, width, height):
@@ -65,7 +85,12 @@ def placeholder(request, width, height):
         return HttpResponseBadRequest('Invalid Image Request')
 
 def index(request):
-    return HttpResponse('Hello World')
+    example = reverse('placeholder', kwargs={'width': 50, 'height': 50})
+    context = {
+        'example': request.build_absolute_uri(example)
+    }
+
+    return render(request, 'home.html', context)
 
 urlpatterns = (
     url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
