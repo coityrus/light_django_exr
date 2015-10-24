9d1cfb24fc533d5c280708ea9f5c605608da4032
Sung-nyoun Kim <tityrus@gmail.com>
2015-10-24 14:07:59 +0900

    commit
    commit

--- static_site_generator/prototypes.py
+++ static_site_generator/prototypes.py
@@ -4,7 +4,6 @@ import sys
 from django.conf import settings
 
 BASE_DIR = os.path.dirname(__file__)
-
 settings.configure(
     DEBUG=True,
     SECRET_KEY='b0mqvak1p2sqm6p#+8o8fyxf+ox(le)8&jh_5^sxa!=7!+wxj0',
